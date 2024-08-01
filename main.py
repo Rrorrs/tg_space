11500a8bda853e98611b5611943746ab775d1352
Rrorrs <110845373+Rrorrs@users.noreply.github.com>
2024-08-01 19:04:03 +0300

    Создала скрипт с остановкой времени и добавила аргумент для кс

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fetch_spacex_images import fetch_spacex_last_launch, get_spacex_images
 from apod_images import add_day_images, get_apod_images
 from epic_images import get_epic, get_image_epic
 from dotenv import load_dotenv
+from tg_bot import time_stop
 
 
 nasa_url = 'https://apod.nasa.gov/apod/image/2402/Ngc1566_HubbleWebb_960.jpg'
@@ -27,13 +28,13 @@ def add_image(nasa_url, filename):
 
 if __name__ == "__main__":
     load_dotenv()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--id', help='необходимый id запуска')
-    args = parser.parse_args()
+    parser_spacex = argparse.ArgumentParser()
+    parser_spacex.add_argument('--id', help='необходимый id запуска')
+    args_spacex = parser_spacex.parse_args()
     try:
-        print(args.id)
-        if args.id:
-            images = get_spacex_images(args.id)
+        print(args_spacex.id)
+        if args_spacex.id:
+            images = get_spacex_images(args_spacex.id)
             fetch_spacex_last_launch(images)
         else:
             images = get_spacex_images(id='latest')
@@ -43,12 +44,25 @@ if __name__ == "__main__":
     except requests.exceptions.HTTPError as error:
         print(error)
 
-    token = os.environ['TOKEN']
-    image_format(nasa_url)
-    filename = f'images/image_nasa.{image_format(nasa_url)}'
-    add_image(nasa_url, filename)
-    image_format(nasa_url)
-    response_apod = get_apod_images(token)
-    add_day_images(response_apod)
-    response_epic = get_image_epic(token)
-    get_epic(response_epic)
+    # token = os.environ['TOKEN']
+    # image_format(nasa_url)
+    # filename = f'images/image_nasa.{image_format(nasa_url)}'
+    # add_image(nasa_url, filename)
+    # image_format(nasa_url)
+    # response_apod = get_apod_images(token)
+    # add_day_images(response_apod)
+    # response_epic = get_image_epic(token)
+    # get_epic(response_epic)
+
+    parser_tg_time = argparse.ArgumentParser()
+    parser_tg_time.add_argument('--time', help='промежуток времени (в секундах), обозначающий интервал, через который будут отправляться фотографии ботом')
+    args_time = parser_tg_time.parse_args()
+    print(args_time.time)
+    try:
+        if args_time.time:
+            time_stop(args_time.time)
+        else:
+            time_stop(stop=10)
+
+    except requests.exceptions.HTTPError as error:
+        print(error)
--- tg_bot.py
+++ tg_bot.py
@@ -1,10 +1,22 @@
 import telegram
 import os
 from dotenv import load_dotenv
+import random
+from PIL import Image
+import time
 
-load_dotenv()
-tk = os.environ['BOT_TOKEN']
-bot = telegram.Bot(token=tk)
-a = bot.get_me()
 
-bot.send_photo(chat_id='@space_nas', photo=open('images/nasa_apod_0.jpg', 'rb'))
+def time_stop(stop):
+    load_dotenv()
+    tk = os.environ['BOT_TOKEN']
+    bot = telegram.Bot(token=tk)
+    while True:
+        folder = os.walk("images")
+        for folder_parametrs in folder:
+            dirpath, dirnames, filenames = folder_parametrs
+        random.shuffle(filenames)
+        for photo in filenames:
+            image = Image.open(f'images/{photo}')
+            bot.send_photo(chat_id='@space_nas', photo=open(f'images/{photo}', 'rb'))
+            time.sleep(stop)
+
