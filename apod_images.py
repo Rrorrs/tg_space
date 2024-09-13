4d34a75acc88580be7f789c847dfe1bc4d6f9ed8
Rrorrs <110845373+Rrorrs@users.noreply.github.com>
2024-09-13 18:49:45 +0300

    Добавила аргумент для выбора количества фоток

--- apod_images.py
+++ apod_images.py
@@ -1,11 +1,11 @@
 import requests
 import os
+import argparse
 from dotenv import load_dotenv
-load_dotenv()
 
 
-def get_apod_images(token):
-    payload = {'api_key': f'{token}', 'count': 30}
+def get_apod_images(token, count_load):
+    payload = {'api_key': f'{token}', 'count': count_load}
     response = requests.get(f'https://api.nasa.gov/planetary/apod', params=payload)
     response.raise_for_status()
     return response
@@ -24,8 +24,15 @@ def add_day_images(response_apod):
             file.write(response.content)
 
 if __name__=='__main__':
-
-    token = os.environ['NASA_TOKEN']
-    days = []
-    response_apod = get_apod_images(token)
-    add_day_images(response_apod)
+    load_dotenv()
+    parser = argparse.ArgumentParser(description='поиск фотографий с API NASA и выгрузка их в телеграмм с помощью бота, можно настроить время интервала между выкладыванием фото (по умолчанию 4 часа). Для spacex нужен id номер конкретного запуска ракеты, иначе будет найден последний запуск.')
+    parser.add_argument('--count_load', help='количество фотографий, которое нужно выгрузить', default=30)
+    args = parser.parse_args()
+    try:
+        token = os.environ['NASA_TOKEN']
+        days = []
+        response_apod = get_apod_images(token, args.count_load)
+        add_day_images(response_apod)
+    
+    except requests.exceptions.HTTPError as error:
+        print(error)
