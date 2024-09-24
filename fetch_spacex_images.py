9f2415762e2a5f5d6f6c7aa82a5bbffcf2393b01
Rrorrs <110845373+Rrorrs@users.noreply.github.com>
2024-09-24 18:50:19 +0300

    Восстановила скрип

---
+++ fetch_spacex_images.py
@@ -0,0 +1,33 @@
+import requests
+import argparse
+import os
+from main import create_path
+from dotenv import load_dotenv
+load_dotenv()
+
+
+token = os.environ['NASA_TOKEN']
+
+
+def get_spacex_link(id):
+    response = requests.get(f'https://api.spacexdata.com/v5/launches/{id}')
+    response.raise_for_status()
+    images = response.json()['links']['flickr']['original']
+    return images
+
+
+def spacex_assigned_numbers(images):
+    for image_number, image in enumerate(images):
+        file_name = 'spacex'
+        create_path(file_name, image, image_number)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='поиск фотографий с API NASA и выгрузка их в телеграмм с помощью бота, можно настроить время интервала между выкладыванием фото (по умолчанию 4 часа). Для spacex нужен id номер конкретного запуска ракеты, иначе будет найден последний запуск.')
+    parser.add_argument('--id', help='необходимый id запуска', default='5eb87d46ffd86e000604b388')
+    args = parser.parse_args()
+    try:
+        images = get_spacex_link(args.id)
+        spacex_assigned_numbers(images)
+    except requests.exceptions.HTTPError as error:
+        print(error)
