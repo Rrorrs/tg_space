d8a838c045b883fb16583ebddc83b3c30e9dca14
Rrorrs <110845373+Rrorrs@users.noreply.github.com>
2024-10-31 18:57:34 +0300

    убрала лишнее

--- create_path_for_images.py
+++ create_path_for_images.py
@@ -12,7 +12,3 @@ def create_a_folder_with_photo(file_name, image, image_number, *args):
         response.raise_for_status()
         with open(f'images/{file_name}_{image_number}.jpg', 'wb') as file:
             file.write(response.content)
-
-
-if __name__ == "__main__":
-    create_a_folder_with_photo()    
