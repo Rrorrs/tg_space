4ecf6ba9889f56290b37a21654d17e09bde36584
Rrorrs <110845373+Rrorrs@users.noreply.github.com>
2024-11-12 19:24:05 +0300

    добавила аргумент

--- tg_bot.py
+++ tg_bot.py
@@ -8,7 +8,7 @@ import argparse
 
 
 
-def launching_the_bot(stop):
+def launching_the_bot(stop, id_address):
     load_dotenv()
     tg_token = os.environ['TG_BOT_TOKEN']
     bot = telegram.Bot(token=tg_token)
@@ -18,17 +18,18 @@ def launching_the_bot(stop):
             dirpath, dirnames, filenames = folder_parametrs
         random.shuffle(filenames)
         for photo in filenames:
-            bot.send_photo(chat_id='@space_nas', photo=open(f'images/{photo}', 'rb'))
+            bot.send_photo(chat_id=f'@{id_address}', photo=open(f'images/{photo}', 'rb'))
             time.sleep(stop)
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Выгрузка фотографий в телеграмм с помощью бота, можно настроить время интервала между выкладыванием фото (по умолчанию 4 часа). ')
     parser.add_argument('--time', default=14400, help='промежуток времени (в секундах), обозначающий интервал, через который будут отправляться фотографии ботом', type=int)
+    parser.add_argument('--tg_id', help='ваш адрес телеграмма, на который будут отправляться фото')
     args = parser.parse_args()
     try:
-        launching_the_bot(args.time)
-
-
+        launching_the_bot(stop=args.time, id_address=args.tg_id)
     except requests.exceptions.HTTPError as error:
         print('Введен неверный тип данных, время должно записываться только числами')
+
+    
